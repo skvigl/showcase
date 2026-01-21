5a649c10a6b87aa0e2bd7f453ab120655d6a00fa
skvigl <skvigl@gmail.com>
2026-01-21 17:53:46 +0500

    API2: Update prisma schema

--- api2/prisma/migrations/20260120083011_create_event/migration.sql
+++ api2/prisma/migrations/20260120083011_create_event/migration.sql
@@ -2,6 +2,8 @@
 CREATE TABLE "events" (
     "id" TEXT NOT NULL,
     "name" VARCHAR(64) NOT NULL,
+    "start_date" TIMESTAMP(3),
+    "end_date" TIMESTAMP(3),
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
     "deleted_at" TIMESTAMP(3),
