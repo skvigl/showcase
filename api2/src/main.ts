f172c9da2fab9d78ab79b152381ab23d4932a3a0
skvigl <skvigl@gmail.com>
2026-03-24 23:03:24 +0500

    Update configs

--- api2/src/main.ts
+++ api2/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { Logger } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
 
 import { AppModule } from './app.module';
+
+export function httpLogger(req: Request, res: Response, next: NextFunction) {
+  Logger.log(`${req.method} ${req.originalUrl}`, 'HTTP');
+  next();
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +25,7 @@ async function bootstrap() {
     credentials: true,
   });
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(httpLogger);
+  await app.listen(process.env.PORT ?? 3000, process.env.HOST ?? 'localhost');
 }
 bootstrap();
